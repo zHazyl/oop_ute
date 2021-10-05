bb078037e9b92105ceae844a4a8890d76d7758ed
zHazyl <tnh210302@gmail.com>
2021-10-05 11:07:11 +0700

    added folder CardBank_bt3

---
+++ CardBank_bt3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CardBank_bt3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
